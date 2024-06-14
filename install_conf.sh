e9c8b93d05399e4731b04344a55c611e8152a44d
Vinícius Moura <83794355+ViniciusCruzMoura@users.noreply.github.com>
2024-06-14 12:57:04 -0400

    Update install_conf.sh

--- install_conf.sh
+++ install_conf.sh
@@ -3,13 +3,10 @@
 mkdir -p ~/.vim/pack/gruvbox/start/
 git clone --depth 1 https://github.com/morhetz/gruvbox.git ~/.vim/pack/gruvbox/start/gruvbox
 
-cd ~/
-wget https://raw.githubusercontent.com/ViniciusCruzMoura/dotfiles/master/.vimrc
-wget https://raw.githubusercontent.com/ViniciusCruzMoura/dotfiles/master/.tmux.conf
-
 curl -o ~/.vimrc https://raw.githubusercontent.com/ViniciusCruzMoura/dotfiles/master/.vimrc
 curl -o ~/.tmux.conf https://raw.githubusercontent.com/ViniciusCruzMoura/dotfiles/master/.tmux.conf
 
 curl -o ~/.vim/colors/base16-grayscale-dark.vim --create-dirs https://raw.githubusercontent.com/chriskempson/base16-vim/master/colors/base16-grayscale-dark.vim
 curl -o ~/.vim/colors/base16-gruvbox-dark-hard.vim --create-dirs https://raw.githubusercontent.com/chriskempson/base16-vim/master/colors/base16-gruvbox-dark-hard.vim
 curl -o ~/.vim/colors/gruvbox.vim --create-dirs https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim
+curl -o ~/.vim/colors/256_noir.vim --create-dirs https://raw.githubusercontent.com/andreasvc/vim-256noir/master/colors/256_noir.vim
