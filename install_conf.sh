26adf74f1e60558d5857e73517e6d01c3970825c
Vinícius Moura <83794355+ViniciusCruzMoura@users.noreply.github.com>
2024-06-14 12:17:12 -0400

    Update install_conf.sh

--- install_conf.sh
+++ install_conf.sh
@@ -12,3 +12,4 @@ curl -o ~/.tmux.conf https://raw.githubusercontent.com/ViniciusCruzMoura/dotfile
 
 curl -o ~/.vim/colors/base16-grayscale-dark.vim --create-dirs https://raw.githubusercontent.com/chriskempson/base16-vim/master/colors/base16-grayscale-dark.vim
 curl -o ~/.vim/colors/base16-gruvbox-dark-hard.vim --create-dirs https://raw.githubusercontent.com/chriskempson/base16-vim/master/colors/base16-gruvbox-dark-hard.vim
+curl -o ~/.vim/colors/gruvbox.vim --create-dirs https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim
