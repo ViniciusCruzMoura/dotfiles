53053b955dc5e3672ff4534b0e4d2b78167991c5
Vinícius Moura <83794355+ViniciusCruzMoura@users.noreply.github.com>
2024-07-04 12:18:03 -0400

    Update install_conf.sh

--- install_conf.sh
+++ install_conf.sh
@@ -3,7 +3,7 @@ curl -o ~/.vimrc https://raw.githubusercontent.com/ViniciusCruzMoura/dotfiles/ma
 curl -o ~/.tmux.conf https://raw.githubusercontent.com/ViniciusCruzMoura/dotfiles/master/.tmux.conf
 curl -o ~/.vim/colors/base16-grayscale-dark.vim --create-dirs https://raw.githubusercontent.com/chriskempson/base16-vim/master/colors/base16-grayscale-dark.vim
 curl -o ~/.vim/colors/base16-gruvbox-dark-hard.vim --create-dirs https://raw.githubusercontent.com/chriskempson/base16-vim/master/colors/base16-gruvbox-dark-hard.vim
-yes | sudo pacman -S lxterminal \
+yes | sudo pacman -S --needed lxterminal \
 	arandr \
 	gpicview \
 	pavucontrol \
