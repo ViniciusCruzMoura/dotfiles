83d08d30204b629fdf53b6b62f03af37ab59c613
Vinícius Moura <83794355+ViniciusCruzMoura@users.noreply.github.com>
2024-07-04 12:05:56 -0400

    Update install_conf.sh

--- install_conf.sh
+++ install_conf.sh
@@ -3,3 +3,39 @@ curl -o ~/.vimrc https://raw.githubusercontent.com/ViniciusCruzMoura/dotfiles/ma
 curl -o ~/.tmux.conf https://raw.githubusercontent.com/ViniciusCruzMoura/dotfiles/master/.tmux.conf
 curl -o ~/.vim/colors/base16-grayscale-dark.vim --create-dirs https://raw.githubusercontent.com/chriskempson/base16-vim/master/colors/base16-grayscale-dark.vim
 curl -o ~/.vim/colors/base16-gruvbox-dark-hard.vim --create-dirs https://raw.githubusercontent.com/chriskempson/base16-vim/master/colors/base16-gruvbox-dark-hard.vim
+yes | sudo pacman -S lxterminal \ 
+	arandr \
+	gpicview \
+	pavucontrol \
+	pulseaudio \
+	nitrogen \
+	blueman \
+	bluez \
+	bluez-utils \
+	nmcli \
+	nmtui \
+	networkmanager \
+	tlp \
+	tlp-rdw \
+	smartmontools \
+	ethtool \
+	xfce4-power-manager \
+	chromium \
+	notepadqq \
+	git \
+	tmux \
+	vlc \
+	pcmanfm \
+	neofetch \
+	ttf-hack \
+	ttc-iosevka \
+	xfce4-screenshooter \
+	gnome-calculator \
+	transmission \
+	libreoffice-fresh \
+	libreoffice-fresh-pt-br \
+	rofi \
+	unrar \
+	openconnect \
+	xfce4-clipman \
+	xarchiver
