bc8552b0ceac88b9816ac3c53c990137b3a29815
Vinícius Moura <83794355+ViniciusCruzMoura@users.noreply.github.com>
2024-07-03 23:06:30 -0400

    Update and rename ideapad3r7.sh to setup.sh

--- .screenlayout/ideapad3r7.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output Virtual1 --primary --mode 800x600 --pos 0x0 --rotate normal
---
+++ .screenlayout/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+if [ grep "HDMI-1 disconnected" ]; then
+    xrandr --output Virtual1 --primary --mode 1360x768 --output HDMI-1 --off
+else
+    xrandr --output Virtual1 --primary --auto --output HDMI-1 --auto
+fi
