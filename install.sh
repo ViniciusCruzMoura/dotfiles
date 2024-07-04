aee949b78d496247ea307b9187a31e6a84e79751
Vinícius Moura <83794355+ViniciusCruzMoura@users.noreply.github.com>
2024-07-04 17:58:26 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ curl -o ~/.vimrc https://raw.githubusercontent.com/ViniciusCruzMoura/dotfiles/ma
 curl -o ~/.tmux.conf https://raw.githubusercontent.com/ViniciusCruzMoura/dotfiles/master/.tmux.conf ;
 curl -o ~/.vim/colors/base16-grayscale-dark.vim --create-dirs https://raw.githubusercontent.com/chriskempson/base16-vim/master/colors/base16-grayscale-dark.vim ;
 curl -o ~/.vim/colors/base16-gruvbox-dark-hard.vim --create-dirs https://raw.githubusercontent.com/chriskempson/base16-vim/master/colors/base16-gruvbox-dark-hard.vim ;
+sudo pacman -Syu ;
 sudo pacman -S --needed lxterminal \
 	arandr \
 	gpicview \
@@ -39,3 +40,4 @@ sudo pacman -S --needed lxterminal \
 curl -o ~/.screenlayout/setup.sh --create-dirs https://raw.githubusercontent.com/ViniciusCruzMoura/dotfiles/master/.screenlayout/setup.sh ;
 curl -o ~/.config/i3/config --create-dirs https://raw.githubusercontent.com/ViniciusCruzMoura/dotfiles/master/i3/config ;
 curl -o ~/.i3status.conf --create-dirs https://raw.githubusercontent.com/ViniciusCruzMoura/dotfiles/master/.i3status.conf ;
+echo -e "You need config the \n1.Lxterminal, \n2.Dbeaver, \n3.Postman, \n4.Cisco, \n5.Ssh-keys, \n6.Docker, \n7.Note files, \n8.Browser links"
