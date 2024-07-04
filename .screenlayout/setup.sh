6d3ad8499b8bd07e8d0e9bc8f383cd7f21357244
Vinícius Moura <83794355+ViniciusCruzMoura@users.noreply.github.com>
2024-07-04 12:46:33 -0400

    Update setup.sh

--- .screenlayout/setup.sh
+++ .screenlayout/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-if  xrandr | grep -q "HDMI-1 disconnected"; then
+if xrandr | grep -q "HDMI-1 disconnected"; then
     xrandr --output Virtual1 --primary --mode 1360x768 --output HDMI-1 --off
+elif xrandr | grep -q "HDMI-1 connected"; then
+    xrandr --output Virtual1 --off --output HDMI-1 --auto --primary
 else
     xrandr --output Virtual1 --primary --auto --output HDMI-1 --auto
 fi
