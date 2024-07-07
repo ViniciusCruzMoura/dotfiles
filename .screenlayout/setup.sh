0d625241e697e822eee76c4dbac1ad2a2d0be5b9
Vinícius Moura <83794355+ViniciusCruzMoura@users.noreply.github.com>
2024-07-07 18:24:20 -0400

    Update setup.sh

--- .screenlayout/setup.sh
+++ .screenlayout/setup.sh
@@ -3,7 +3,6 @@ if xrandr | grep -q "HDMI-1 disconnected"; then
     #xrandr --output eDP-1 --primary --mode 1280x720 --output HDMI-1 --off
     xrandr --output eDP-1 --primary --auto --scale-from 1360x768 --output HDMI-1 --off
 elif xrandr | grep -q "HDMI-1 connected"; then
-    #xrandr --output eDP-1 --off --output HDMI-1 --auto --primary
     xrandr --output eDP-1 --off --output HDMI-1 --auto --primary --scale-from 1360x768
 else
     xrandr --output eDP-1 --primary --auto --output HDMI-1 --auto
