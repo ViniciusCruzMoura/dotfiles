9d4da8004d9dcd089a080c44398e0ed2230e6a03
Vinícius Moura <83794355+ViniciusCruzMoura@users.noreply.github.com>
2024-07-06 21:24:00 -0400

    Update setup.sh

--- .screenlayout/setup.sh
+++ .screenlayout/setup.sh
@@ -8,3 +8,9 @@ elif xrandr | grep -q "HDMI-1 connected"; then
 else
     xrandr --output eDP-1 --primary --auto --output HDMI-1 --auto
 fi
+
+#xrandr | grep -w connected | cut -f '1' -d ' ' | while read -r line ; do
+#    if xrandr | grep -q "$line disconnected"; then
+#        xrandr --output $line --off
+#    fi
+#done
